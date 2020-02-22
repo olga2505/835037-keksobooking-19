cd65824825ffc26d6b60125f8a2ded4d92c185d2
Olga <151590@mail.ru>
2020-02-22 20:24:38 +0300

    доверяй, но проверяй (часть 1) с правками 8

--- js/main.js
+++ js/main.js
@@ -237,7 +237,7 @@ var onPinMainMousedown = function (evt) {
 
 var onPinMainKeydown = function (evt) {
   if (evt.key === ENTER_KEY) {
-    getAddresAndUnlockForm();
+    getAddressAndUnlockForm();
   }
 };
 
